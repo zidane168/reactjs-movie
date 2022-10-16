5b47c47bacd0b11ca4c3c0e1efed81cb72cb7f85
zidane <huuvi168@gmail.com>
2022-10-17 06:56:08 +0700

    init movies sourcecode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+import searchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com/?apikey=7f4fff21&s=movie';
+ 
+// {"Search":[{"Title":"Movie Movie","Year":"1978","imdbID":"tt0077952","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BOTJlMDIxN2YtYmU0NS00ZjIxLWFiMjgtNjlkZTQ2Y2I1NDQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjI4MjA5MzA@._V1_SX300.jpg"},{"Title":"Films of Fury: The Kung Fu Movie Movie","Year":"2011","imdbID":"tt1779401","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTUzODI2ODE4OV5BMl5BanBnXkFtZTgwNDAzNTA2MDE@._V1_SX300.jpg"},{"Title":"The Movie Movie","Year":"1986","imdbID":"tt0226165","Type":"movie","Poster":"N/A"},{"Title":"The Return of the Movie Movie","Year":"1972","imdbID":"tt1373217","Type":"movie","Poster":"N/A"}],"totalResults":"4","Response":"True"}
+
+// const movies = [
+//   {
+//     "Title":"Movie Movie",
+//     "Year":"1978",
+//     "imdbID":"tt0077952",
+//     "Type":"movie",
+//     "Poster":"https://m.media-amazon.com/images/M/MV5BOTJlMDIxN2YtYmU0NS00ZjIxLWFiMjgtNjlkZTQ2Y2I1NDQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjI4MjA5MzA@._V1_SX300.jpg"
+//   }
+// ]
 
 function App() {
+
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const [ movies, setMovies ] = useState([])
+  
+  const searchMovie = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+    
+    setMovies(data["Search"])
+  }
+
+  useEffect( () => {
+    searchMovie('movie')
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Movie by Zidane </h1>
+
+      <div className="search justify-content-center">
+        <div className="input-group">
+          <input placeholder="Search for movie" className="control" 
+            onChange={ (e) => { setSearchTerm(e.target.value) } }
+          />
+          <img src={ searchIcon } alt="search" className="searchIcon" 
+            onClick={ () => { searchMovie(searchTerm) } }
+          />
+        </div>
+      </div>
+      
+      {
+        movies?.length > 0 ? 
+          movies.map((movie, index) => {
+
+            return (
+              <div className="movie" key={index}>
+                <p> { movie.Year } </p>
+                <p> { movie.Title } </p>
+                <p> { movie.Type } </p>
+                <img src={ movie.Poster } alt={ movie.title } />
+                <p> { movie.Year } </p>
+              </div>
+            )
+          }) : 
+          <>
+            <h1> Movie Not found </h1>
+          </>
+      }
     </div>
   );
 }
 
 export default App;
+
+// https://www.youtube.com/watch?v=b9eMGE7QtTk
